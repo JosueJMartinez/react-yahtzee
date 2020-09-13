25b8453187f8ad3eeb0fb38e4529d6079bdec9c0
JosueJMartinez <jmarti61@gmail.com>
2020-09-12 23:07:49 -0500

    updated and fixed bug of locking device

--- src/js/components/Game.jsx
+++ src/js/components/Game.jsx
@@ -44,8 +44,9 @@ class Game extends Component {
 		}));
 	}
 
-	toggleLocked(idx) {
+	toggleLocked = idx => {
 		// toggle whether idx is in locked or not
+
 		this.setState(st => ({
 			locked: [
 				...st.locked.slice(0, idx),
@@ -53,7 +54,7 @@ class Game extends Component {
 				...st.locked.slice(idx + 1)
 			]
 		}));
-	}
+	};
 
 	doScore(rulename, ruleFn) {
 		// evaluate this ruleFn with the dice and score this rulename
