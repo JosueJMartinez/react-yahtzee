e405e6d43289e63413cd6c108c250b5c36a3ad91
JosueJMartinez <jmarti61@gmail.com>
2020-09-13 21:58:19 -0500

    refactored made font-awesome local

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './js/App';
 import registerServiceWorker from './registerServiceWorker';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/js/components/RuleRow.jsx
+++ src/js/components/RuleRow.jsx
@@ -4,16 +4,16 @@ import '../../css/RuleRow.css';
 class RuleRow extends Component {
 	render() {
 		const { isRolling, doScore, name, description, score } = this.props;
-		const disable = score !== undefined;
+		const disabled = score !== undefined;
 		return (
 			<tr
-				className={`RuleRow RuleRow-${disable || isRolling
+				className={`RuleRow RuleRow-${disabled || isRolling
 					? 'disabled'
 					: 'active'}`}
 				onClick={doScore}
 			>
 				<td className="RuleRow-name">{name}</td>
-				<td className="RuleRow-score">{disable ? score : description}</td>
+				<td className="RuleRow-score">{disabled ? score : description}</td>
 			</tr>
 		);
 	}
